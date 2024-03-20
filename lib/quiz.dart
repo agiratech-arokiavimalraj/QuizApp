b9310aab9d4d9f2b31b848633e3ab83ecf71d508
agiratech-arokiavimalraj <vimalraj.t@agiratech.com>
2024-03-20 18:56:26 +0530

    today Wednesday

---
+++ lib/quiz.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+import 'package:quiz_app/start_screen.dart';
+import 'package:quiz_app/questions.dart';
+
+class Quiz extends StatefulWidget {
+  const Quiz({super.key});
+  @override
+  State<Quiz> createState() {
+    return _QuizState();
+  }
+}
+
+class _QuizState extends State<Quiz> {
+  Widget? activeScreen;
+
+  @override
+  void initState() {
+    activeScreen = StartScreen(switchScreen);
+    super.initState();
+  }
+
+  void switchScreen() {
+    setState(() {
+      activeScreen = const Questions();
+    });
+  }
+
+  @override
+  Widget build(context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: Container(
+          decoration: const BoxDecoration(
+            gradient: LinearGradient(
+              colors: [
+                Color.fromARGB(255, 255, 17, 0),
+                Color.fromARGB(255, 227, 90, 90),
+              ],
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight,
+            ),
+          ),
+          child: activeScreen,
+        ),
+      ),
+    );
+  }
+}
